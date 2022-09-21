9607fa0c917466a005ef16a7f971d4e4dc453875
rainysteven1 <rainysteven1@gmail.com>
2022-09-21 16:57:33 +0800

    项目初始化

---
+++ cloud-api/cloud-api-system/src/test/java/com/rainy/visualizationcloud/system/api/CloudApiSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rainy.visualizationcloud.system.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudApiSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
